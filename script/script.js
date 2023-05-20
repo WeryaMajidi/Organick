0f866eeb0c7f83ea9656fecfa6a6da433e733aa7
Werya Majidi <109464323+WeryaMajidi@users.noreply.github.com>
2023-05-20 18:04:42 +0430

    :iphone: update CSS for Mobile landscape responsiveness

--- script/script.js
+++ script/script.js
@@ -1,13 +1,15 @@
 const menu = document.querySelector(".toggle-menu");
 const showMenu = document.querySelector(".humberger-menu");
 const hideMenu = document.querySelector(".toggle-close");
+const menuUl = document.querySelector(".header_nav");
 
 showMenu.addEventListener("click", () => {
-  console.log("show");
   menu.classList.add("visible");
 });
 
 hideMenu.addEventListener("click", () => {
-  console.log("hide");
   menu.classList.remove("visible");
+});
+menuUl.addEventListener("click", (e) => {
+  if (e.target.closest(".header_nav-item")) menu.classList.remove("visible");
 });
